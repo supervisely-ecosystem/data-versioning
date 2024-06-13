e0f2dcac11eff430854d94a61352a2fb8623976c
GoldenAnpu <seryios.sych@gmail.com>
2024-06-13 23:05:46 +0200

    Refactor globals.py and main.py for TASK_ID support

--- src/globals.py
+++ src/globals.py
@@ -8,8 +8,14 @@ if sly.is_development():
     load_dotenv("local.env")
     load_dotenv(os.path.expanduser("~/supervisely.env"))
 
+
 PROJECT_ID = int(os.getenv("PROJECT_ID"))
 api: sly.Api = sly.Api.from_env()
+
+if sly.is_development():
+    TASK_ID = int(os.getenv("TASK_ID"))
+else:
+    TASK_ID = int(api.task_id)
 
 action = "create" if bool(strtobool(os.environ.get("modal.state.create"))) else "restore"
 if action == "create":
--- src/main.py
+++ src/main.py
@@ -12,11 +12,11 @@ def main():
             project_info, g.version_title, g.version_description
         )
         g.api.app.set_output_text(
-            g.api.task_id, "New restore point created.", zmdi_icon="zmdi-time-restore"
+            g.TASK_ID, "New restore point created.", zmdi_icon="zmdi-time-restore"
         )
     else:
         new_project_info = g.api.project.version.restore(project_info, g.target_version)
-        g.api.app.set_output_project(g.api.task_id, new_project_info.id, new_project_info.name)
+        g.api.app.set_output_project(g.TASK_ID, new_project_info.id, new_project_info.name)
     diff = timer.get_sec()
     sly.logger.debug(f"Project version {g.action} took {diff:.2f} sec")
 
