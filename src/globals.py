3cfee92788024f935d775c31da9851bdd4ba26a0
GoldenAnpu <seryios.sych@gmail.com>
2024-06-13 22:42:34 +0200

    Refactor app

--- src/globals.py
+++ src/globals.py
@@ -1,4 +1,5 @@
 import os
+from distutils.util import strtobool
 
 import supervisely as sly
 from dotenv import load_dotenv
@@ -10,4 +11,12 @@ if sly.is_development():
 PROJECT_ID = int(os.getenv("PROJECT_ID"))
 api: sly.Api = sly.Api.from_env()
 
-download_dir = os.path.join(os.getcwd(), "download")
+action = "create" if bool(strtobool(os.environ.get("modal.state.create"))) else "restore"
+if action is "create":
+    version_title = str(os.environ.get("modal.state.versionTitle"))
+    version_description = str(os.environ.get("modal.state.versionDescription"))
+    target_version = None
+elif action is "restore":
+    version_title = None
+    version_description = None
+    target_version = int(os.environ.get("modal.state.targetVersion"))
--- src/main.py
+++ src/main.py
@@ -1,79 +1,22 @@
-import os
-
 import supervisely as sly
-import zstd
-from supervisely.io.fs import archive_directory
-from tqdm import tqdm
+from supervisely.tiny_timer import TinyTimer
 
-from functions import create_project_map, diff_and_update_maps, download_project
-from globals import PROJECT_ID, api, download_dir
+import globals as g
 
 
 def main():
-    project_info = api.project.get_info_by_id(PROJECT_ID)
-    cur_version = project_info.custom_data.get("project-version", None)
-    cur_project_mapping = project_info.custom_data.get("project-mapping", None)
-
-    if cur_version is None:
-        version = 1
-    else:
-        version = int(cur_version) + 1
-    project_info.custom_data["project-version"] = version
-
-    project_mapping = create_project_map(api, PROJECT_ID, version)
-
-    if cur_project_mapping is not None:
-        project_mapping, item_ids, deleted_items = diff_and_update_maps(
-            cur_project_mapping, project_mapping, version
+    project_info = g.api.project.get_info_by_id(g.PROJECT_ID)
+    timer = TinyTimer()
+    if g.action == "create":
+        project_info = g.api.project.version.create(
+            project_info, g.version_title, g.version_description
         )
+        g.api.app.set_output_text("New restore point created.", zmdi_icon="zmdi-time-restore")
     else:
-        item_ids = None
-        deleted_items = None
-
-    if item_ids is None or len(item_ids) > 0:
-        if os.path.exists(download_dir):
-            sly.fs.remove_dir(download_dir)
-        download_project(api, project_info, download_dir, item_ids=item_ids)
-    elif deleted_items is not None and len(deleted_items) > 0:
-        if not os.path.exists(download_dir):
-            os.makedirs(download_dir, exist_ok=True)
-    else:
-        sly.logger.info(
-            "You already have the latest snapshot of the project. No need to backup it again."
-        )
-        return
-
-    json_map_path = os.path.join(download_dir, "mapping.json")
-    sly.json.dump_json_file(project_mapping, json_map_path)
-
-    archive_path = os.path.join(os.path.dirname(download_dir), "download.tar")
-    archive_directory(download_dir, archive_path)
-    zst_archive_path = archive_path + ".zst"
-    with open(archive_path, "rb") as tar:
-        with open(zst_archive_path, "wb") as zst:
-            zst.write(zstd.ZSTD_compress(tar.read()))
-
-    remote_path = f"/PVS/{PROJECT_ID}/{PROJECT_ID}_v{version}.tar.zst"
-
-    progress_cb = tqdm(
-        desc=f"Creating backup version v{version}",
-        total=os.path.getsize(zst_archive_path),
-        unit="B",
-        unit_scale=True,
-    )
-    uploade_file_info = api.file.upload(
-        project_info.team_id, zst_archive_path, remote_path, progress_cb
-    )
-    progress_cb.close()
-
-    project_info.custom_data["project-mapping"] = project_mapping
-    api.project.update_custom_data(PROJECT_ID, project_info.custom_data, silent=True)
-
-    sly.fs.silent_remove(archive_path)
-    sly.fs.silent_remove(zst_archive_path)
-    sly.fs.remove_dir(download_dir)
-
-    api.app.set_output_directory(api.task_id, uploade_file_info.id, os.path.dirname(remote_path))
+        new_project_info = g.api.project.version.restore(project_info, g.target_version)
+        g.api.app.set_output_project(new_project_info.id)
+    diff = timer.get_sec()
+    sly.logger.debug(f"Project version {g.action} took {diff:.2f} sec")
 
 
 if __name__ == "__main__":
