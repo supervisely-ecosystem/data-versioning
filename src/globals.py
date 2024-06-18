eaa8cdca262223ee892c9e1c4689258a5680fa29
GoldenAnpu <seryios.sych@gmail.com>
2024-06-19 01:37:31 +0200

    Refactor requirements.txt to use workflow+versions instead of project-versioning

--- src/globals.py
+++ src/globals.py
@@ -20,7 +20,11 @@ else:
 action = "create" if bool(strtobool(os.environ.get("modal.state.create"))) else "restore"
 if action == "create":
     version_title = str(os.environ.get("modal.state.versionTitle"))
+    if version_title == "":
+        version_title = None
     version_description = str(os.environ.get("modal.state.versionDescription"))
+    if version_description == "":
+        version_description = None
     target_version = None
 elif action == "restore":
     version_title = None
--- src/main.py
+++ src/main.py
@@ -8,9 +8,10 @@ def main():
     project_info = g.api.project.get_info_by_id(g.PROJECT_ID)
     timer = TinyTimer()
     if g.action == "create":
-        project_info = g.api.project.version.create(
+        project_version = g.api.project.version.create(
             project_info, g.version_title, g.version_description
         )
+        g.api.app.add_output_project(project_info, project_version)
         g.api.app.set_output_text(
             g.TASK_ID,
             "New restore point created",
@@ -19,6 +20,7 @@ def main():
         )
     else:
         new_project_info = g.api.project.version.restore(project_info, g.target_version)
+        g.api.app.add_output_project(new_project_info)
         g.api.app.set_output_project(g.TASK_ID, new_project_info.id, new_project_info.name)
     diff = timer.get_sec()
     sly.logger.debug(f"Project version {g.action} took {diff:.2f} sec")
