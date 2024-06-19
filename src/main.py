94abd26522c728bdeb22814a181fdd5074e332be
GoldenAnpu <seryios.sych@gmail.com>
2024-06-19 16:40:25 +0200

    Update output message

--- src/main.py
+++ src/main.py
@@ -7,25 +7,32 @@ import globals as g
 def main():
     project_info = g.api.project.get_info_by_id(g.PROJECT_ID)
     timer = TinyTimer()
+    if project_info.version:
+        version_id = project_info.version.get("id")
+        version_num = project_info.version.get("version")
+    else:
+        version_id, version_num = None, None
     if g.action == "create":
         project_version_id = g.api.project.version.create(
             project_info, g.version_title, g.version_description
         )
-        if project_info.version and project_version_id == project_info.version.get("id"):
+        if project_info.version and project_version_id == version_id:
+            link = f"{g.api.server_address}/projects/{project_info.id}"
             g.api.app.set_output_text(
                 g.TASK_ID,
-                "There is no changes in the project. New restore point was not created.",
-                description="Version",
+                f"There is no changes in the project <a href={link}>{project_info.name}</a>",
+                description=f"New restore point was not created",
                 zmdi_icon="zmdi-close-circle",
                 icon_color="#FFA500",
                 background_color="#FFE8BE",
             )
         else:
+            link = f"{g.api.server_address}/projects/{project_info.id}/versions"
             g.api.app.add_output_project(project_info, project_version_id)
             g.api.app.set_output_text(
                 g.TASK_ID,
                 "New restore point created",
-                description="Version",
+                description=f"<a href={link}>Version {version_num + 1}</a>",
                 zmdi_icon="zmdi-time-restore",
             )
     else:
