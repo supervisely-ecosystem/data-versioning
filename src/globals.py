e5df5bf60954ef0081224cbccbb4eaafdc75d727
GoldenAnpu <seryios.sych@gmail.com>
2024-06-26 16:04:46 +0200

    Refactor modal window

--- src/globals.py
+++ src/globals.py
@@ -17,19 +17,19 @@ if sly.is_development():
 else:
     TASK_ID = int(api.task_id)
 
-action = "create" if bool(strtobool(os.environ.get("modal.state.create"))) else "restore"
+action = os.environ.get("modal.state.actionType")
 if action == "create":
-    version_title = str(os.environ.get("modal.state.versionTitle"))
+    version_title = str(os.environ.get("modal.state.title"))
     if version_title == "":
         version_title = None
-    version_description = str(os.environ.get("modal.state.versionDescription"))
+    version_description = str(os.environ.get("modal.state.description"))
     if version_description == "":
         version_description = None
     version_num = None
 elif action == "restore":
     version_title = None
     version_description = None
-    version_num = int(os.environ.get("modal.state.targetVersion"))
+    version_num = int(os.environ.get("modal.state.version"))
 
 
 create_meta = {"customNodeSettings": {"title": "<h4>Create New Version</h4>"}}
