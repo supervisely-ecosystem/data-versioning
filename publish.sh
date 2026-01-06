7301ec4113ba97ce82955368f2c6313af24577ac
Sergey <57998637+GoldenAnpu@users.noreply.github.com>
2026-01-06 13:26:14 +0100

    Add latest tag to Docker build and push commands

--- publish.sh
+++ publish.sh
@@ -1,2 +1,3 @@
-docker build -t supervisely/data-versioning:6.73.493 . && \
-docker push supervisely/data-versioning:6.73.493
+docker build -t supervisely/data-versioning:6.73.493 -t supervisely/data-versioning:latest . && \
+docker push supervisely/data-versioning:6.73.493 && \
+docker push supervisely/data-versioning:latest
